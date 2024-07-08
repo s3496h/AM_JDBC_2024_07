7738af126bc035e9ac7c1e6a249542dc77c03055
ãs3496h <s3496h@gmail.com>
2024-07-08 16:36:27 +0900

    글 작성 시 DB에 INSERT 되도록

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -1,5 +1,6 @@
 package org.koreait;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -32,7 +33,51 @@ public class Main {
                 System.out.println(article);
 
                 lastArticleId++;
-            }else if(cmd.equals("article list")) {
+                System.out.println(id + "번 글이 작성되었습니다.");
+                Connection conn = null;
+                PreparedStatement pstmt = null;
+                ResultSet rs = null;
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/AM_JDBC_2024_07?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
+                    System.out.println("연결 성공!");
+
+                    String sql = "INSERT INTO article ";
+                    sql += "SET regDate = NOW(),";
+                    sql += "updateDate = NOW(),";
+                    sql += "title = CONCAT('제목', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),";
+                    sql += "`body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2));";
+
+                    System.out.println(sql);
+
+                    pstmt = conn.prepareStatement(sql);
+
+                    int affectedRows = pstmt.executeUpdate();
+
+                    System.out.println("affected rows: " + affectedRows);
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else if (cmd.equals("article list")) {
                 System.out.println("==목록==");
                 if(articles.size() == 0){
                     System.out.println("게시글이 없습니다");
