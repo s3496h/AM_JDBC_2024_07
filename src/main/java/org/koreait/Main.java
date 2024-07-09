2c560653e99a0b4bcc1a37f5d14025b42535a86b
ãs3496h <s3496h@gmail.com>
2024-07-09 15:48:15 +0900

    게시글 수정 기능 구현, 글의 유무 체크 x

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -14,7 +14,9 @@ public class Main {
 
 
         while (true) {
+            System.out.print("명령어 : ");
             String cmd = sc.nextLine().trim();
+
             if (cmd.equals("exit")) {
 
                 break;
@@ -102,10 +104,10 @@ public class Main {
                         String updateDate = rs.getString("updateDate");
                         String title = rs.getString("title");
                         String body = rs.getString("body");
-                        Article article = new Article(id,regDate,updateDate,title,body);
+                        Article article = new Article(id, regDate, updateDate, title, body);
                         articles.add(article);
                     }
-                    for (int i = 0 ; i < articles.size() ; i++ ){
+                    for (int i = 0; i < articles.size(); i++) {
                         System.out.println("번호 : " + articles.get(i).getId());
                         System.out.println("작성 날짜 : " + articles.get(i).getRegDate());
                         System.out.println("수정 날짜 : " + articles.get(i).getUpdateDate());
@@ -144,14 +146,77 @@ public class Main {
                         e.printStackTrace();
                     }
                 }
-                    if(articles.size() == 0){
+                if (articles.size() == 0) {
                     System.out.println("게시글이 없습니다");
                     continue;
                 }
                 System.out.println("   번호    /    제목     ");
-                for(Article article : articles){
+                for (Article article : articles) {
                     System.out.printf("     %d      /    %s    \n", article.getId(), article.getTitle());
 
+                }
+            } else if (cmd.startsWith("article modify")) {
+                int id = 0;
+
+                try {
+                    id = Integer.parseInt(cmd.split(" ")[2]);
+                } catch (Exception e) {
+                    System.out.println("정수 입력");
+                    continue;
+                }
+                System.out.println("==글수정==");
+                System.out.print("새 제목 : ");
+                String title = sc.nextLine();
+                System.out.print("새 내용 : ");
+                String body = sc.nextLine();
+
+                Connection conn = null;
+                PreparedStatement pstmt = null;
+
+                try {
+                    Class.forName("org.mariadb.jdbc.Driver");
+                    String url = "jdbc:mariadb://127.0.0.1:3306/AM_JDBC_2024_07?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+                    conn = DriverManager.getConnection(url, "root", "");
+                    System.out.println("연결 성공!");
+
+                    String sql = "UPDATE article ";
+                    sql += "SET updateDate = NOW()";
+                    if (title.length() > 0) {
+                        sql += " , title = '" + title + "'";
+                    }
+                    if (body.length() > 0) {
+                        sql += " , `body` = '" + body + "'";
+                    }
+                    sql += " WHERE id = " + id + ";";
+
+                    System.out.println(sql);
+
+                    pstmt = conn.prepareStatement(sql);
+
+                    pstmt.executeUpdate();
+
+
+                } catch (ClassNotFoundException e) {
+                    System.out.println("드라이버 로딩 실패" + e);
+                } catch (SQLException e) {
+                    System.out.println("에러 : " + e);
+                } finally {
+                    try {
+                        if (conn != null && !conn.isClosed()) {
+                            conn.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        if (pstmt != null && !pstmt.isClosed()) {
+                            pstmt.close();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+
+                    }
+                    System.out.println(id + "번 글이 수정되었습니다.");
                 }
 
             }
